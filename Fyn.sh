4a6c2595b3f95299c635dd6669e24b91af692407
Riemayuni <53336657+Riemayuni@users.noreply.github.com>
2019-10-31 10:00:42 +0700

    Add files via upload

---
+++ Fyn.sh
@@ -0,0 +1,264 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya bosQiu 
+echo
+echo
+echo
+echo
+clear
+figlet 17 TOOLS | lolcat
+echo $i"["$bi"•"$i"]"$me"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$i"["$bi"•"$i"]"
+echo $ku" |"$ku"     INDONESIA DARK TERMUX INDONESIA"$ku"         |"
+echo $ku" |"$me"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$ku"|"
+echo $ku" |"$pu" AUTHOR :"$bi" RIFARIF'ARIFYN "$ku"                    |"
+echo $ku" |"$pu" FACEBOOK :"$bi" RifArif'Arifyn "$ku"                  |"
+echo $ku" |"$pu" THANKS TO :"$pur"~ [RIEMAYUNITA] [MR.4RIF] "$ku"       |"
+echo $ku" |"$pu" Contack WhatsApp :"$bi" 085813915016"$ku"             |"
+echo $ku" |"$pu" Contack YAHOO :"$bi" Riemayunitha@yahoo.com"$ku"      |"
+echo $i"["$bi"•"$i"]"$me"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$i"["$bi"•"$i"]"
+echo
+echo $pur"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    ~~~~~"
+echo $pur"|"$me" 1"$pur" |"$me" BRUTEFORCE FB PHP        "$pur"|"$pur"    |"$ku" ✪"$pur" |"
+echo $pur"|"$me" 2"$pur" |"$pu" BRUTEFORCE FB BR1G4D3    "$pur"|"$pur"    |"$cy" T"$pur" |"
+echo $pur"|"$me" 3"$pur" |"$i" BRUTEFORCE FB            "$pur"|"$pur"    |"$cy" O"$pur" |"
+echo $pur"|"$me" 4"$pur" |"$bi" BRUTEFORCE FB MBF        "$pur"|"$pur"    |"$cy" O"$pur" |"
+echo $pur"|"$me" 5"$pur" |"$cy" FACEBRUTE                "$pur"|"$pur"    |"$cy" L"$pur" |"
+echo $pur"|"$me" 6"$pur" |"$ku" BRUTEFORCE FB ( root )   "$pur"|"$pur"    |"$cy" S"$pur" |"
+echo $pur"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$pur"    |"$ku" ✪"$pur" |"
+echo $pur"|"$me" 7"$pur" |"$me" YAHOO CLONING FACEBOOK   "$pur"|"$pur"    ~~~~~"
+echo $pur"|"$me" 8"$pur" |"$ku" PROFILE GUARD FACEBOOK   "$pur"|"$pur"    |"$ku" ✪"$pur" |"
+echo $pur"|"$me" 9"$pur" |"$i" REPORT FACEBOOK WORK     "$pur"|"$pur"    |"$cy" T"$pur" |"
+echo $pur"|"$me"10"$pur" |"$bi" AUTO REACTION FACEBOOK   "$pur"|"$pur"    |"$cy" E"$pur" |"
+echo $pur"|"$me"11"$pur" |"$cy" INFORMATION FACEBOOK     "$pur"|"$pur"    |"$cy" R"$pur" |"
+echo $pur"|"$me"12"$pur" |"$pu" JADWAL SHOLAT            "$pur"|"$pur"    |"$cy" M"$pur" |"
+echo $pur"|"$me"13"$pur" |"$ku" AUTOLIKE FACEBOOK        "$pur"|"$pur"    |"$cy" U"$pur" |"
+echo $pur"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$pur"    |"$cy" X"$pur" |"
+echo $pur"|"$me"14"$pur" |"$bi" PHISING FACEBOOK 1       "$pur"|"$pur"    |"$ku" ✪"$pur" |"
+echo $pur"|"$me"15"$pur" |"$i" PHISING FACEBOOK 2       "$pur"|"$pur"    ~~~~~"
+echo $pur"|"$me"16"$pur" |"$ku" PHISING FACEBOOK 3       "$pur"|"$pur"    |"$cy" K"$pur" |"
+echo $pur"|"$me"17"$pur" |"$me" PHISING FACEBOOK 4       "$pur"|"$pur"    |"$cy" E"$pur" |"
+echo $pur"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$pur"    |"$cy" R"$pur" |"
+echo $pur"|"$me"18"$pur" |"$cy" INSTALL BAHAN DULU bosQiu"$pur"|"$pur"    |"$cy" E"$pur" |"
+echo $pur"|"$me"19"$pur" |"$cy" KELUAR/EXIT PROGRAM      "$pur"|"$pur"    |"$cy" N"$pur" |"
+echo $pur"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    ~~~~~"
+echo
+echo $me"┌==="$bi"["$i"FRIENDS"$bi"]"$me"======"$bi"["$i""Pilih Nomornya bosQiu""$bi"]"
+echo $me"¦"
+read -p"└──>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/novalattasya/drkfb
+cd drkfb
+python2 DarkFB.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Jadwal-Sholat
+cd Jadwal-Sholat
+sh jadwal.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 085813915016"
+echo $ku"Facebook :"$i" RIFARIF'ARIFYN"
+sleep 2
+echo $pur">> Terima kasih Yang Sudah Support Saya <<"
+exit
+fi
